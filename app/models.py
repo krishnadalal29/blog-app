36771d2dbb433ded7f8ed81a984f7ff32a7cf2cc
Krishna <krishnasukhadiya24@gmail.com>
2025-05-24 13:16:58 -0700

    Rotes and templates

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from . import db
+from flask_login import UserMixin, current_user
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
---
+++ app/routes.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template
+from .forms import LoginForm
+from .models import Post, User
+import os
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+def index():
+    posts = Post.query.all()
+    return render_template("frame_wrapper.html", page="base", posts=posts)
+
+@main.route("/templates/<filename>")
+def templates(filename):
+    template_path = f"{filename}.html"
+    if os.path.exists(os.path.join("app/templates/", template_path)):
+        if filename == "index":
+            posts = Post.query.all()
+            return render_template("frame_wrapper.html", page=filename, posts=posts)
+        elif filename == "login":
+            form = LoginForm()
+            return render_template("frame_wrapper.html", page=filename, form=form)
+        elif filename == "register":
+            return render_template("frame_wrapper.html", page=filename)
+        else:
+            return render_template("frame_wrapper.html", page=filename)
+    else:
+        return f"Page '{filename}' not found.", 404
+
+@main.route("/login", methods=["POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            print("Login successful")
+            return render_template("frame_wrapper.html",page="index", posts=Post.query.all())
+        else:
+            print("Login failed")
+            form.username.errors.append("Invalid username or password")
+            return render_template("frame_wrapper.html", page="login", form=form, error=form.errors)
+    else:
+        return render_template("frame_wrapper.html", page="login", form=form, error=form.errors)
